f30c3a48dd560d9820832bb179890eed985aa344
O-orion <lucas.lvtorres@gmail.com>
2024-06-14 17:41:32 -0300

    ajustando endline

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,10 @@
+import { StyleSheet } from 'react-dom'
 
 export default function Home() {
     return (
-        <h1>
-            Primeiro Nextjs
+        <h1 >
+            Primeira page Nextjs
         </h1>
     )
 }
+
