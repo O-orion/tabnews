2def0b29b8f41f125f4cf44d5030ab1ae853126a
O-orion <lucas.lvtorres@gmail.com>
2024-06-28 18:23:20 -0300

    styles

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,17 @@
 import React from 'react'
 import { StyleSheet } from 'react-dom'
 
+
 export default function Home() {
     return (
-        <h1 >
+        <h1 class={styles.text} >
             Esse carlão é muito viado!
         </h1>
     )
 }
 
+const styles =  StyleSheet.create({
+    text: {
+        color: 'red'
+    }
+})
